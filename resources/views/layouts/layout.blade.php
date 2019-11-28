548fe996a392b24031303652d8bab251e7669c99
Daniel <43178677+Dohko19@users.noreply.github.com>
2019-11-28 02:18:28 -0600

    routes

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -26,7 +26,7 @@
   <nav class="navbar navbar-transparent navbar-color-on-scroll fixed-top navbar-expand-lg" color-on-scroll="100" id="sectionsNav">
     <div class="container">
       <div class="navbar-translate">
-        <a class="navbar-brand" href="#">
+        <a class="navbar-brand" href="{{ route('index') }}">
           {{ config('app.name', 'Laravel') }} </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
           <span class="sr-only">Navegación</span>
