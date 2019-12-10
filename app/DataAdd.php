d988a8699e132302d791416ebcc4a1bcb8ee416c
Daniel <43178677+Dohko19@users.noreply.github.com>
2019-12-10 01:25:15 -0600

    Files

--- app/DataAdd.php
+++ app/DataAdd.php
@@ -6,7 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class DataAdd extends Model
 {
-	protected $fillable = ['user_id', 'rfc', 'cedula_profesional', 'certificacion', 'permisos_especiales', 'otro', 'business_image', 'tiempo_completo', 'servicios_externos', 'domicilio', 'atencion_inmediata'];
+	protected $fillable = [
+		'user_id', 'rfc', 'cedula_profesional', 'certificacion', 'permisos_especiales', 'otro', 'business_image', 'tiempo_completo', 'servicios_externos', 'domicilio', 'atencion_inmediata'
+						];
+
     public function user()
     {
     	return $this->belongsTo(User::class);
